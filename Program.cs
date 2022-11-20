f89bf1d7d6694272116d601a52992b74dcd3f895
unknown <zarbaliev@code.edu.az>
2022-11-20 03:07:42 -0800

    Homework Class

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Homework;
+#region Student
+//Student student = new Student();
+//student.Name = "Amid";
+//student.Surname = "imanov";
+//student.Group = "T-203";
+//student.Graduated = "Yes";
+//student.Point = 90;
+
+//Console.WriteLine(student.Point);
+
+//Student student2 = new Student();
+//student2.Name = "Humay";
+//student2.Surname = "Aliyeva";
+//student2.Group = "RDM";
+//student2.Graduated = "No";
+//student2.Point = 75;
+//Console.WriteLine(student2.Graduated);
+
+//Student student3 = new Student("Elnur", "Veliyev", "Fil-fak", "NO",);
+//Console.WriteLine(student3.Name);
+////Student.Pointt();
+#endregion  
+static string Write (string name)
+{
+    return "Elvira Imanova";
+}
+string numSrt = Write(string name);
+Console.WriteLine("name");
---
+++ Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+#region Student
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public string Graduated;
+        public byte Point;
+
+        public Student(string name, string surname, string group, string graduated, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Graduated = graduated;
+            Point = point;
+
+        }
+        public void Pointt()
+
+        {
+            if (Point > 80) ;
+            {
+                Console.WriteLine("Imtahana icaze verilir");
+
+            }
+
+            if (Point < 80) ;
+            {
+                Console.WriteLine("Imtahana icaze verilmir");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+#endregion 
